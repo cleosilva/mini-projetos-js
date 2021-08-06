96e1b35da038fe65fb179e64f4b11a7442224a03
Cleonir Silva <82469705+cleosilva@users.noreply.github.com>
2021-08-06 20:28:32 -0300

    Add files via upload

---
+++ quiz/questions.js
@@ -0,0 +1,102 @@
+let questions = [
+    {
+        question: 'Como se chama o gato da Dona Clotilde?',
+        options: [
+            'Madruguinha',
+            'Fofinho',
+            'Satanás',
+            'Bola de Neve'
+        ],
+        answer: 2
+    },
+    {
+        question: 'Em qual episódio Chaves diz: “Eu prefiro morrer do que perder a vida”?',
+        options: [
+            'O banho do Chaves',
+            'Bombinhas são perigosas, ainda mais em mãos erradas',
+            'Nas pontas dos pés',
+            'Guerra às crianças'
+        ],
+        answer: 1
+    },
+    {
+        question: 'Qual o nome do cãozinho de pelúcia da Chiquinha?',
+        options: [
+            'Peludinho',
+            'Satanás',
+            'Madruguinha',
+            'Canela'
+        ],
+        answer: 0
+    },
+    {
+        question: 'Complete a frase: Eu prometo que no ano que vem...',
+        options: [
+            'Me caso com Dona Florinda',
+            'Vou emprestar meus brinquedos',
+            'Terá 12 meses',
+            'Vou me comportar bem'
+        ],
+        answer: 2
+    },
+    {
+        question: 'O que o Chaves atropela com a bicicleta?',
+        options: [
+            'Quico',
+            'Madruguinha',
+            'O gato do Quico',
+            'Satanás'
+        ],
+        answer: 2
+    },
+    {
+        question: 'Seu Madruga já trabalhou em vários empregos. Em qual desses ele não trabalhou?',
+        options: [
+            'Leiteiro',
+            'Entregador de lenha',
+            'Agente especializado em compra e venda de artigos para o lar',
+            'Garçom'
+        ],
+        answer: 3
+    },
+    {
+        question: 'Quem levou Chaves a Acapulco?',
+        options: [
+            'Seu Madruga',
+            'Seu Barriga',
+            'Dona Clotilde',
+            'Professor Girafales'
+        ],
+        answer: 1
+    },
+    {
+        question: 'Qual o nome que Chaves dá para a bola de boliche?',
+        options: [
+            'Bola de Colito',
+            'Corpo de Benito',
+            'Bola de Boliche',
+            'Bola do Benito'
+        ],
+        answer: 1
+    },
+    {
+        question: 'Seu Madruga ganha um terno de herança de qual falecido parente?',
+        options: [
+            'Primo Vicente',
+            'Tio Benito',
+            'Tio Jacinto',
+            'Tia Iris'
+        ],
+        answer: 2
+    },
+    {
+        question: 'Em qual série da escola estudam as crianças?',
+        options: [
+            '3ª A',
+            '1ª C',
+            '4ª B',
+            '2ª B'
+        ],
+        answer: 3
+    },
+];
